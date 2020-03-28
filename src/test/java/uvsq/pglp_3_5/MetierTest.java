3f03d0a55d71d511f05d8942a1c05e514c87dacd
Maher Attouche <maherattouche7@gmail.com>
2020-03-28 16:49:29 +0100

    Junit tests

---
+++ src/test/java/uvsq/pglp_3_5/MetierTest.java
@@ -0,0 +1,14 @@
+package uvsq.pglp_3_5;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MetierTest {
+
+	@Test
+	public void testLogger() {
+		assertTrue(true);
+	}
+
+}
